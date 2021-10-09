fac1cabb7ee1aaf331eb63f76150d3c02c0c95fb
Sami Dakhani <samidakhani@gmail.com>
2021-10-09 16:28:58 -0400

    Adding auto approve and merge

--- src/main/java/org/automate/actions/automateactions/AutomateActionsApplication.java
+++ src/main/java/org/automate/actions/automateactions/AutomateActionsApplication.java
@@ -22,6 +22,7 @@ public class AutomateActionsApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		logger.info("This is an application for github actions.");
 		logger.info("Initiating manual pull request");
+		logger.info("Initiating automated pull request");
 	}
 
 }
