e9de225eb08d803545860626da647e0111fab276
Sami Dakhani <samidakhani@gmail.com>
2021-10-09 13:35:52 -0400

    Updating the code to make it work

--- src/main/java/org/automate/actions/automateactions/AutomateActionsApplication.java
+++ src/main/java/org/automate/actions/automateactions/AutomateActionsApplication.java
@@ -1,13 +1,23 @@
 package org.automate.actions.automateactions;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class AutomateActionsApplication {
+public class AutomateActionsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutomateActionsApplication.class, args);
+		SpringApplication application = new SpringApplication(AutomateActionsApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("This is an application for github actions.");
 	}
 
 }
